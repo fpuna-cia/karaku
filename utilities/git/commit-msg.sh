b489d8c6befd55499291c82b93725f22c6ac4560
Arturo Volpe Torres <avolpe@pol.una.py>
2013-04-11 10:59:21 -0400

    Modificaciones de scripts de hook y del template del commit.
    Se modifica el script que configuraba el template del commit en este
    archivo se modifica la url para sincronizarla con la nueva estructura
    del proyecto
    Se modifica el archivo del template de los commits para agregar un
    enter entre el cuerpo del mensaje y la referencia
    
    refs #2501

--- utilities/git/commit-msg.sh
+++ utilities/git/commit-msg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd utilities/git
-chmod +x hooks/commit-msg
-cp hooks/commit-msg ../../.git/hooks/commit-msg
+
+#cd base/utilities/git
+chmod +x base/utilities/git/hooks/commit-msg
+cp base/utilities/git/hooks/commit-msg .git/hooks/commit-msg
 git config commit.template utilities/git/.gitmessage.txt
