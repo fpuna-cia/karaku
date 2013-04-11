bbb3e3c94ec1d6855a25f46d028bce9a8acbc544
Arturo Volpe Torres <avolpe@pol.una.py>
2013-04-11 11:24:03 -0400

    Actualizacion del mensaje de commit
    Se cambia el formato del commit para agregar una linea en blanco entre
    el cuerpo del mensaje y la referencia, en este commit se modifica el
    path a las plantillas basicas de commit
    
    refs #2501

--- utilities/git/commit-msg.sh
+++ utilities/git/commit-msg.sh
@@ -3,4 +3,4 @@
 #cd base/utilities/git
 chmod +x base/utilities/git/hooks/commit-msg
 cp base/utilities/git/hooks/commit-msg .git/hooks/commit-msg
-git config commit.template utilities/git/.gitmessage.txt
+git config commit.template base/utilities/git/.gitmessage.txt
