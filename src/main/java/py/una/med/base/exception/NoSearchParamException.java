aa7ea8deb33eba207578a4c2c7c18d8fd8e23b4a
Diego Acuna <dacuna@pol.una.py>
2013-04-26 10:34:33 -0400

    Excepcion de valor de busqueda
    NoSearchParamException : excepcion lanzada en tiempo de ejecución en el
    caso de que no fuera ingresado un parametro de busqueda en la busqueda
    simple.
    
    refs #2551
    
    Signed-off-by: Uriel Gonzalez <ugonzalez@pol.una.py>

---
+++ src/main/java/py/una/med/base/exception/NoSearchParamException.java
@@ -0,0 +1,23 @@
+/*
+ * @NoSearchParamException.java 1.0 Abr 26, 2013 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.exception;
+
+/**
+ * 
+ * @author Diego Acuña
+ * @since 1.0
+ * @version 1.0 Abr 26, 2013
+ * 
+ */
+public class NoSearchParamException extends RuntimeException {
+	
+	private static final long serialVersionUID = -154211713138448380L;
+
+	public NoSearchParamException (String key) {
+
+		super(key);
+	}
+	
+}
