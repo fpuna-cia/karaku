29312fe9f541ed255747f32cbd1590d737da5749
Uriel Gonzalez <ugonzalez@pol.una.py>
2013-06-26 10:51:36 -0400

    Agrega la lista de codigos de respuesta del HTTP.
    Agrega una lista de constantes de codigos de respuesta del HTTP. Los
    codigos de estatus estan especificados en el RFC 2616.
    
    refs #3067
    
    Signed-off-by: Uriel Gonzalez <ugonzalez@pol.una.py>

---
+++ src/main/java/py/una/med/base/util/HTTPStatusCode.java
@@ -0,0 +1,15 @@
+package py.una.med.base.util;
+
+/**
+ * Lista de codigos de respuesta del HTTP. Estos codigos de estatus estan
+ * especificados en el RFC 2616.
+ * 
+ * @author Uriel Gonzalez
+ * 
+ */
+public final class HTTPStatusCode {
+
+	// El servidor no ha encontrado nada que coincida con el Request-URI
+	public static final String NOT_FOUND = "404";
+
+}
