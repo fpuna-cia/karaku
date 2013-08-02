8099068e9ac87755908b54020426cd2f47f52af6
Arturo Volpe <avolpe@pol.una.py>
2013-08-02 08:14:54 -0400

    Agregacicion de convertidor estandar de cadenas
    Se agrega un FacesConverter que se comporta igual que el por defecto,
    el objetivo es para aquellos lugares donde no se requiera que se pase
    a mayúsculas o no estan relacionados a un atributo particular, como
    es el caso del basic_filter que tenia problemas con el nuevo
    converter.
    
    refs #4111

---
+++ src/main/java/py/una/med/base/jsf/converters/DefaultStringConverter.java
@@ -0,0 +1,53 @@
+/*
+ * @KarakuConverter.java 1.0 Jun 6, 2013 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.jsf.converters;
+
+import java.lang.reflect.Field;
+import javax.el.ValueExpression;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import py.una.med.base.dao.annotations.CaseSensitive;
+import py.una.med.base.exception.KarakuRuntimeException;
+import py.una.med.base.util.ELHelper;
+
+/**
+ * Es un {@link FacesConverter} que no realiza ninguna acción, es utilizado
+ * cuando no se desea utilizar el {@link StringConverter}, su uso es asi:
+ * 
+ * <pre>
+ * &lt;h:selectOneMenu "
+ * converter = &quot;oldStringConverter&quot; /&gt;
+ *     ...
+ * &lt;/h:selectOneMenu&gt;
+ * </pre>
+ * 
+ * @author Arturo Volpe
+ * @since 1.3.2
+ * @version 1.0 Jun 6, 2013
+ * 
+ */
+@FacesConverter(value = "oldStringConverter")
+public class DefaultStringConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+
+		return value;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object object) {
+
+		if (object == null) {
+			return "";
+		}
+		return object.toString();
+	}
+
+}
