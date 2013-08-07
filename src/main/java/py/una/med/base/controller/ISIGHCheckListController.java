988e3f8a67fb73c1fcd0235ffcba2c3497c06540
ovianconi <ovianconi@pol.una.py>
2013-08-07 07:22:11 -0400

    Actualizacion -Base- CU Autorizacion de Solicitud Plan de Atencion
    Actualizacion del CU para integrar el nuevo comportamiento.
    Es decir, permitir la seleccion de solicitudes desde la grilla (list.xhtml)
    mediante checkboxs y luego ir al formulario en donde se completan los demas
    campos requeridos para la autorizacion.
    
    refs #4081
    
    Signed-off-by: Uriel Gonzalez <ugonzalez@pol.una.py>

---
+++ src/main/java/py/una/med/base/controller/ISIGHCheckListController.java
@@ -0,0 +1,47 @@
+/*
+ * @ISIGHCheckList.java 1.0 24/07/2013 Sistema Integral de Gestion Hospitalaria
+ */
+package py.una.med.base.controller;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import javax.faces.event.AjaxBehaviorEvent;
+import py.una.med.base.dynamic.forms.MultiplePickerField.ItemKeyProvider;
+import py.una.med.base.util.SIGHConverterV2;
+
+/**
+ * 
+ * @author Osmar Vianconi
+ * @since 1.0
+ * @version 1.0 24/07/2013
+ * 
+ */
+public interface ISIGHCheckListController<T, ID extends Serializable> extends
+		ISIGHAdvancedController<T, ID> {
+
+	Collection<T> getSelected();
+
+	Object getItemKeyProvider(T item);
+
+	void setItemKeyProvider(ItemKeyProvider<T> itemKeyProvider);
+
+	HashMap<T, Boolean> getSelectedMap();
+
+	void setSelectedMap(HashMap<T, Boolean> selectedMap);
+
+	boolean isSelectAllButtonVisible();
+
+	void setSelectAllButtonVisible(boolean buttonDisabled);
+
+	void setSelected(Collection<T> selected);
+
+	void onCheckboxHeaderClicked(final AjaxBehaviorEvent event);
+
+	void setChecked(T object);
+
+	@Override
+	String getDefaultPermission();
+
+	SIGHConverterV2 getConverter();
+}
