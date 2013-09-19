3c8ff6b271c759bc125998944ac6953663066469
Arturo Volpe <avolpe@pol.una.py>
2013-09-19 09:44:35 -0400

    Test para StringUtils y documentacion
    Se agregan test para verificar la validez de la calse StringUtils y
    además se actualiza la documentación de la misma.
    
    refs #4852
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>

--- src/main/java/py/una/med/base/util/StringUtils.java
+++ src/main/java/py/una/med/base/util/StringUtils.java
@@ -22,20 +22,21 @@ public final class StringUtils {
 		// No-op
 	}
 
-	private static List<String> Vocales = Arrays.asList("a", "e", "i", "o",
-			"u", "A", "E", "I", "O", "U");
-	private static List<String> VocalesFuertesTonicas = Arrays.asList("á", "é",
-			"ó");
-	private static List<String> VocalesDebilesTonicas = Arrays.asList("í", "ú");
-	private static List<String> ConsonantesEspeciales = Arrays.asList("d", "j",
-			"l", "n", "r", "D", "J", "L", "N", "R");
-	private static List<String> Consonantes = Arrays.asList("b", "c", "d", "f",
-			"g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v",
-			"w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L",
-			"M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z");
-	private static List<String> Mayus = Arrays.asList("A", "B", "C", "D", "E",
-			"F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-			"S", "T", "U", "V", "W", "X", "Y", "Z");
+	private static List<Character> Vocales = Arrays.asList('a', 'e', 'i', 'o',
+			'u', 'A', 'E', 'I', 'O', 'U');
+	private static List<Character> VocalesFuertesTonicas = Arrays.asList('á',
+			'é', 'ó');
+	private static List<Character> VocalesDebilesTonicas = Arrays.asList('í',
+			'ú');
+	private static List<Character> ConsonantesEspeciales = Arrays.asList('d',
+			'j', 'l', 'n', 'r', 'D', 'J', 'L', 'N', 'R');
+	private static List<Character> Consonantes = Arrays.asList('b', 'c', 'd',
+			'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
+			'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K',
+			'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z');
+	private static List<Character> Mayus = Arrays.asList('A', 'B', 'C', 'D',
+			'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+			'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 
 	/**
 	 * Valida si una cadena es o no válida (tomando en cuenta cadenas recibidas
@@ -84,33 +85,109 @@ public final class StringUtils {
 		return true;
 	}
 
+	/**
+	 * Valida si una cadena es o no inválida (tomando en cuenta cadenas
+	 * recibidas desde la interfaz), una cadena es válida si no es
+	 * <code>null</code> y si tiene al menos un carácter distinto de un espacio
+	 * 
+	 * @param string
+	 *            a validar
+	 * @return <code>true</code> si es válida, y <code>false</code> en caso
+	 *         contrario
+	 */
 	public static boolean isInvalid(final String string) {
 
 		return !isValid(string);
 	}
 
+	/**
+	 * Une un grupo de palabras con un separador.
+	 * 
+	 * @param separator
+	 *            cadena que servira de pegamento entre las demás cadenas
+	 * @param strings
+	 *            lista de palabras a unir
+	 * @return una simple cadena compuesta de las demás cadenas separadas por un
+	 *         separador. <code>null</code> si no hay ninguna cadena.
+	 */
 	public static String join(final String separator, final String ... strings) {
 
+		if (separator == null) {
+			throw new IllegalArgumentException("Separator can't be null");
+		}
+		if (strings == null) {
+			return "";
+		}
+		if (strings.length == 0) {
+			return "";
+		}
+		if (strings.length == 1) {
+			return strings[0];
+		}
 		return join(separator, 0, strings.length - 1, strings);
 	}
 
+	/**
+	 * Une un grupo de palabras con un separador.
+	 * 
+	 * @param separator
+	 *            cadena que servirá de pegamento entre las demas cadenas
+	 * @param strings
+	 *            lista de palabras a unir
+	 * @return una simple cadena compuesta de las demás cadenas separadas por un
+	 *         separador. <code>null</code> si no hay ninguna cadena.
+	 */
 	public static String join(final String separator,
 			final Collection<String> strings) {
 
+		if (separator == null) {
+			throw new IllegalArgumentException("Separator can't be null");
+		}
+		if (strings == null) {
+			return "";
+		}
+		if (strings.size() == 0) {
+			return "";
+		}
 		return join(separator, 0, strings.size() - 1,
 				strings.toArray(new String[strings.size()]));
 	}
 
+	/**
+	 * Une un grupo de palabras con un separador.
+	 * 
+	 * @param separator
+	 *            cadena que servirá de pegamento entre las demas cadenas
+	 * @param strings
+	 *            lista de palabras a unir
+	 * @param start
+	 *            primer elemento a unir
+	 * @param end
+	 *            ultimo elemento a unir
+	 * @return una simple cadena compuesta de las demás cadenas separadas por un
+	 *         separador. <code>null</code> si no hay ninguna cadena.
+	 */
 	public static String join(final String separator, final int start,
 			final int end, final String ... strings) {
 
+		if (separator == null) {
+			throw new IllegalArgumentException("Separator can't be null");
+		}
 		if (strings == null) {
 			return "";
 		}
+		if (strings.length == 0) {
+			return null;
+		}
+		if (strings.length == 1) {
+			return strings[0];
+		}
+		int begin = Math.max(start, 0);
+		int to = Math.min(end, strings.length - 1);
 		StringBuilder sb = new StringBuilder();
-		for (int i = start; i <= end; i++) {
+		for (int i = begin; i <= to; i++) {
 			sb.append(strings[i]);
-			if (i < end) {
+			if (i < to) {
 				sb.append(separator);
 			}
 		}
@@ -119,38 +196,45 @@ public final class StringUtils {
 
 	/**
 	 * Dada una cadena escrita en camelCase retorna una lista con los elementos
-	 * que forman la cadena<br>
-	 * <b>Por ejemplo</b><br>
+	 * que forman la cadena
+	 * <p>
+	 * <b>Por ejemplo</b>
 	 * <ol>
-	 * <li><b>HolaMundo</b>, retorna [Hola,Mundo]
+	 * <li><b>HolaMundo</b>, retorna [Hola,Mundo]</li>
 	 * </ol>
 	 * 
-	 * @param string cadena a partir
+	 * </p>
+	 * 
+	 * @param string
+	 *            cadena a partir
 	 * @return list de elementos
 	 */
-	public static List<String> split(final String strings) {
+	public static List<String> split(final String string) {
 
 		List<String> list = new ArrayList<String>();
 		int j = 0;
-		for (int i = 1; i < strings.length(); i++) {
-			if (Mayus.indexOf(strings.charAt(i)) != -1) {
-				list.add(strings.substring(j, i));
+		for (int i = 1; i < string.length(); i++) {
+			if (Mayus.contains(string.charAt(i))) {
+				list.add(string.substring(j, i));
 				j = i;
 			}
 		}
-		list.add(strings.substring(j, strings.length()));
+		list.add(string.substring(j, string.length()));
 		return list;
 	}
 
 	/**
-	 * Dada una cadena retorna el plural de la misma<br>
-	 * <b>Por ejemplo</b><br>
+	 * Dada una cadena retorna el plural de la misma
+	 * <p>
+	 * <b>Por ejemplo</b>
 	 * <ol>
-	 * <li><b>Lapiz</b>, retorna Lapices
-	 * <li><b>Pais</b>, retorna Paises
+	 * <li><b>Lapiz</b>, retorna Lapices</li>
+	 * <li><b>Pais</b>, retorna Paises</li>
 	 * </ol>
+	 * </p>
 	 * 
-	 * @param singular cadena en singular
+	 * @param singular
+	 *            cadena en singular
 	 * @return plural cadena pluralizada
 	 */
 	public static String pluralize(final String singular) {
@@ -174,16 +258,11 @@ public final class StringUtils {
 				return singular;
 			}
 		}
-		if (singular.endsWith("s") || singular.endsWith("S")
-				|| singular.endsWith("x") || singular.endsWith("X")) {
-			if ((singular.length() <= 4) || (singular.length() > 8)) {
-				plural = singular.concat("es");
-				return plural;
-			} else {
-				return singular;
-			}
+		if (is(terminate, 's', 'S')) {
+			plural = singular.concat("es");
+			return plural;
 		}
-		if (singular.endsWith("y") || singular.endsWith("Y")) {
+		if (is(terminate, 'y', 'Y')) {
 			if (Vocales.contains(singular.charAt(singular.length() - 2))) {
 				plural = singular.concat("es");
 				return plural;
@@ -192,7 +271,7 @@ public final class StringUtils {
 				return plural;
 			}
 		}
-		if (singular.endsWith("z") || singular.endsWith("Z")) {
+		if (is(terminate, 'z', 'Z')) {
 			plural = singular.substring(0, singular.length() - 1).concat("ces");
 			return plural;
 		}
@@ -203,12 +282,72 @@ public final class StringUtils {
 		return null;
 	}
 
-	public static String pluralize(final List<String> terms) {
+	private static boolean is(Character first, Character ... c) {
 
+		if (first == null && (c == null || c.length < 1)) {
+			return true;
+		}
+		if (first != null && c.length < 1) {
+			return false;
+		}
+		for (int i = 0; i < c.length; i++) {
+			if (first == c[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Pluraliza una serie de palabras separandolas por un espacio.
+	 * 
+	 * @param terms
+	 *            grupo de palabras a pluralizar
+	 * @return <code>null</code> si no hay terms o terms es <code>null</code>,
+	 *         en otro caso una cadena formada por las cadenas pasadas separadas
+	 *         por un espacio.
+	 * @see #pluralize(String, Collection)
+	 */
+	public static String pluralize(final Collection<String> terms) {
+
+		if (terms == null || terms.size() == 0) {
+			return null;
+		}
 		StringBuilder buf = new StringBuilder();
-		for (String term : terms) {
-			buf.append(pluralize(term));
-			buf.append(" ");
+		String[] array = terms.toArray(new String[terms.size()]);
+		for (int i = 0; i < terms.size(); i++) {
+			buf.append(pluralize(array[i]));
+			if (i + 1 != terms.size()) {
+				buf.append(" ");
+			}
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * Pluraliza una serie de palabras separandolas por un token separador.
+	 * 
+	 * @param terms
+	 *            grupo de palabras a pluralizar
+	 * @param separator
+	 *            separador de palabras
+	 * @return <code>null</code> si no hay terms o terms es <code>null</code>,
+	 *         en otro caso una cadena formada por las cadenas pasadas separadas
+	 *         por un separator.
+	 */
+	public static String pluralize(String separator,
+			final Collection<String> terms) {
+
+		if (terms == null || terms.size() == 0) {
+			return null;
+		}
+		StringBuilder buf = new StringBuilder();
+		String[] array = terms.toArray(new String[terms.size()]);
+		for (int i = 0; i < terms.size(); i++) {
+			buf.append(pluralize(array[i]));
+			if (i + 1 != terms.size()) {
+				buf.append(separator);
+			}
 		}
 		return buf.toString();
 	}
---
+++ src/test/java/py/una/med/base/test/test/StringUtilsTest.java
@@ -0,0 +1,140 @@
+/*
+ * @StringUtilsTest.java 1.0 Sep 19, 2013 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.test.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import py.una.med.base.test.base.BaseTest;
+import py.una.med.base.test.configuration.BaseTestConfiguration;
+import py.una.med.base.util.StringUtils;
+
+/**
+ * 
+ * @author arturo
+ * @since 1.0
+ * @version 1.0 Sep 19, 2013
+ * 
+ */
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class StringUtilsTest extends BaseTest {
+
+	@Configuration
+	static class ContextConfiguration extends BaseTestConfiguration {
+
+	}
+
+	String NULL = null;
+
+	@Test
+	public void testIsValid() {
+
+		assertFalse(StringUtils.isValid("        "));
+		assertFalse(StringUtils.isValid(NULL));
+		assertTrue(StringUtils.isValid("HOLA"));
+
+		assertFalse(StringUtils.isValid(" a ", NULL));
+		assertFalse(StringUtils.isValid(" a ", " "));
+		assertTrue(StringUtils.isValid(" a ", " b "));
+	}
+
+	@Test
+	public void testIsInvalid() {
+
+		assertTrue(StringUtils.isInvalid("        "));
+		assertTrue(StringUtils.isInvalid(NULL));
+		assertFalse(StringUtils.isInvalid("HOLA"));
+
+	}
+
+	@Test
+	public void testJoin() {
+
+		assertEquals(StringUtils.join(",", "hola", "chau"), "hola,chau");
+		assertEquals(StringUtils.join("'''", "hola", "chau"), "hola'''chau");
+		assertEquals(StringUtils.join(" ", "hola", "chau"), "hola chau");
+
+		List<String> cadenas = new ArrayList<String>();
+
+		cadenas.add("hola");
+		cadenas.add("chau");
+
+		assertEquals(StringUtils.join(",", cadenas), "hola,chau");
+		assertEquals(StringUtils.join("'''", cadenas), "hola'''chau");
+		assertEquals(StringUtils.join(" ", cadenas), "hola chau");
+
+		assertEquals(StringUtils.join(",", -1, 2, "hola", "chau"), "hola,chau");
+		assertEquals(StringUtils.join("'''", 0, 2, "hola", "chau"),
+				"hola'''chau");
+		assertEquals(StringUtils.join(" ", 0, 2, "hola", "chau", "ches"),
+				"hola chau ches");
+
+	}
+
+	@Test
+	public void testSplit() {
+
+		List<String> cadenas = new ArrayList<String>();
+
+		cadenas.add("hola");
+		cadenas.add("Chau");
+		assertTrue(comparareList(cadenas, StringUtils.split("holaChau")));
+	}
+
+	@Test
+	public void testPluralize() {
+
+		List<String> cadenas = new ArrayList<String>();
+
+		cadenas.add("hola");
+		cadenas.add("Chau");
+		assertEquals("holas", StringUtils.pluralize("hola"));
+		assertEquals("Chaus", StringUtils.pluralize("Chau"));
+		assertEquals("peces", StringUtils.pluralize("pez"));
+		assertEquals("telares", StringUtils.pluralize("telar"));
+		assertEquals("mamboses", StringUtils.pluralize("mambos"));
+		assertEquals("madams", StringUtils.pluralize("madam"));
+		assertEquals("Lapices", StringUtils.pluralize("Lapiz"));
+		assertEquals("Paises", StringUtils.pluralize("Pais"));
+
+		assertEquals("holas Chaus", StringUtils.pluralize(cadenas));
+		assertEquals("holas-Chaus", StringUtils.pluralize("-", cadenas));
+
+	}
+
+	private <T> boolean comparareList(Collection<T> l1, Collection<T> l2) {
+
+		if (l1 == null && l2 != null) {
+			return false;
+		}
+		if (l2 == null && l1 != null) {
+			return false;
+		}
+		if (l1 == null && l2 == null) {
+			return true;
+		}
+		if (l1.size() != l2.size()) {
+			return false;
+		}
+		boolean flag = true;
+		Object[] a1 = l1.toArray(new Object[l1.size()]);
+		Object[] a2 = l2.toArray(new Object[l2.size()]);
+		for (int i = 0; i < l1.size(); i++) {
+			flag = a1[i].equals(a2[i]);
+			if (!flag) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
