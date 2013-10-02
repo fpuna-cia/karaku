12f854224d7c6478301f900dfa44a15f7711a675
Uriel Gonzalez <ugonzalez@pol.una.py>
2013-10-01 20:06:43 -0400

    Agrega la excepción HTTPException.
    Esta excepción debe ser lanzada cuando ocurre algun error durante el
    procesamiento de solicitudes HTTP.
    
    refs #5073
    
    Signed-off-by: Uriel Gonzalez <ugonzalez@pol.una.py>

---
+++ src/main/java/py/una/med/base/exception/HTTPException.java
@@ -0,0 +1,50 @@
+package py.una.med.base.exception;
+
+/**
+ * Describe una excepcion que ocurre durante el procesamiento de solicitudes
+ * HTTP
+ * 
+ * @author Uriel Gonzalez
+ * 
+ */
+public class HTTPException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	private String code;
+	private String shortDesciption;
+
+	/**
+	 * 
+	 * @param code
+	 *            Codigos de estatus especificados en el RFC 2616
+	 */
+	public HTTPException(String code, String shortDescription) {
+
+		this.code = code;
+		this.shortDesciption = shortDescription;
+	}
+
+	public HTTPException(String code, String shortDescription, Throwable cause) {
+
+		super(cause);
+		this.code = code;
+		this.shortDesciption = shortDescription;
+	}
+
+	/**
+	 * Recupera el codigo de estado de HTTP
+	 * 
+	 * @return codigo de estado
+	 */
+	public String getCode() {
+
+		return this.code;
+	}
+
+	public String getShortDescription() {
+
+		return this.shortDesciption;
+	}
+
+}
