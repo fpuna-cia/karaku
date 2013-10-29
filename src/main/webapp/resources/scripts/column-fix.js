e6e61a6e40b93b1885cff8b64a376bf7ec3bf9b6
Arturo Volpe <avolpe@pol.una.py>
2013-10-29 13:22:37 -0300

    Eliminacion de funete personalizada
    Se elimina la fuente Ubuntu.woff, pues provoca un error y su acceso es
    incorrecto (la url correcta debe ser: ../../resources/fonts/Ubuntu.
    woff).
    
    refs #5682
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>

--- src/main/webapp/resources/scripts/column-fix.js
+++ src/main/webapp/resources/scripts/column-fix.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function() {
 	var datacontainer = $('.rf-edt-c');
 
-	console.log(datacontainer.width());
 	$('.rf-edt-hdr-c-cnt').width(datacontainer.width());
 	$('.rf-edt-c-cnt').width(datacontainer.width());
 });
