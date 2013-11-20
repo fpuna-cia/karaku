18d72bcae9a8b2beb808d29d030702765b28829e
Arturo Volpe <avolpe@pol.una.py>
2013-11-20 10:41:53 -0300

    QuantityContextListener
    Se agrega un QuantityContextListener que se encarga de asignar las
    variables de entorno de la JVM para que las librerias JSF de apache
    no intenten convertir un null a un número (provocando errores, pues
    no conocen la entidad Quantity).
    
    refs #6004
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>
    Signed-off-by: Hector Martinez <hdmartinez@pol.una.py>

---
+++ src/main/java/py/una/med/base/math/QuantityContextListener.java
@@ -0,0 +1,32 @@
+/*
+ * @QuantityContextListener.java 1.0 Nov 20, 2013 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.math;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Context Listener que se encarga de setear las propiedades de entorno
+ * necesarias para el correcto funcionamiento de la entidad Quantity.
+ *
+ * @author Arturo Volpe
+ * @since 2.2.8
+ * @version 1.0 Nov 20, 2013
+ *
+ */
+public class QuantityContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		System.setProperty("org.apache.el.parser.COERCE_TO_ZERO", "false");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+
+	}
+
+}
