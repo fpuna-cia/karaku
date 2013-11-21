44da9a7f97c5bd12d1df1dba4388432747081f59
Arturo Volpe <avolpe@pol.una.py>
2013-11-21 09:33:30 -0300

    DummyReplicationProvider
    Implementacion basica de un ReplicationProvider, que envia todas las
    entidades cada vez que son solicitadas, solamente acepta la cadena
    'ZERO' como ultimo identificador.
    
    refs #5840
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>
    Signed-off-by: Hector Martinez <hdmartinez@pol.una.py>

---
+++ src/main/java/py/una/med/base/replication/DummyReplicationProvider.java
@@ -0,0 +1,62 @@
+/*
+ * @DummyReplicationProvider.java 1.0 Nov 7, 2013 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.replication;
+
+import java.util.List;
+import org.apache.poi.ss.formula.eval.NotImplementedException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * {@link ReplicationProvider} que simplemente envía toda la tabla cada vez que
+ * se solicita un cambio.
+ *
+ * <p>
+ * Solo funciona cuando el id del cambio es {@link Bundle#ZERO_ID}, en caso
+ * contrario lanza una {@link NotImplementedException}</p<
+ *
+ * @author Arturo Volpe
+ * @since 2.2.8
+ * @version 1.0 Nov 7, 2013
+ *
+ */
+@Transactional
+public class DummyReplicationProvider implements ReplicationProvider {
+
+	@Autowired
+	SessionFactory factory;
+
+	@Override
+	@Transactional(readOnly = true)
+	public <T extends Shareable> Bundle<T> getChanges(Class<T> clazz,
+			String lastId) {
+
+		if (Bundle.ZERO_ID.equals(lastId)) {
+			return getAll(clazz);
+		}
+		throw new NotImplementedException(
+				"Dummy Replication provider dont support a ID != 'ZERO'");
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends Shareable> Bundle<T> getAll(Class<T> clazz) {
+
+		List<T> entities = getSession().createCriteria(clazz).list();
+		Bundle<T> toRet = new Bundle<T>();
+		for (T entitie : entities) {
+			toRet.add(entitie, Bundle.ZERO_ID);
+		}
+		return toRet;
+
+	}
+
+	protected Session getSession() {
+
+		return factory.getCurrentSession();
+	}
+
+}
