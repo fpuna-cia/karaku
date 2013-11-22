5ccdf0163594a7c300bf28c3b871896131edc9df
Arturo Volpe <avolpe@pol.una.py>
2013-11-22 10:32:06 -0300

    Checker de parametros
    Se agrega la clase con metodos estaticos Checker, que se encarga de
    validar los atributos pasados a la misma y lanzar excepciones adecuadas.
    Es una copia de versiones populares que realizan lo mismo, como
    
     - Preconditions: de guava
     - Validate: de org.apache.commons.lang3
     - Assert: de spring
    
    Esta clase es mas similar a la clase Assert de Spring, pero incorpora
    caracteristicas de las otras dos, como formateador de fechas, y mas
    metodos. Ademas, la clase Assert de Spring no es recomendada para
    entornos fuera del framework.
    
    La intencion de esta clase es que se agregen metodos para facilitar
    la validacion de parametros y estados de objetos a los desarrolladores.
    
    refs #6062
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>
    Signed-off-by: Hector Martinez <hdmartinez@pol.una.py>

---
+++ src/main/java/py/una/med/base/util/Checker.java
@@ -0,0 +1,50 @@
+/*
+ * @Checker.java 1.0 Nov 21, 2013 Sistema Integral de Gestion Hospitalaria
+ */
+package py.una.med.base.util;
+
+/**
+ * 
+ * @author Arturo Volpe
+ * @since 2.2.8
+ * @version 1.0 Nov 21, 2013
+ * 
+ */
+public class Checker {
+
+	/**
+	 * Chequea si un parámetro es nulo.
+	 * 
+	 * <p>
+	 * Si el parámetro es nulo, entonces lanza una excepción del tipo
+	 * </p>
+	 * 
+	 * @param object
+	 */
+	public static <T> T notNull(T object) {
+
+		return notNull(object, "Object not null is not allowed");
+	}
+
+	public static <T> T notNull(T object, String message, Object ... arguments) {
+
+		if (object == null) {
+			throw new IllegalArgumentException(format(message, arguments));
+		}
+		return object;
+	}
+
+	public static <T extends CharSequence> T notValid(T string, String message,
+			Object ... arguments) {
+
+		if ((string == null) || StringUtils.isInvalid(string.toString())) {
+			throw new IllegalArgumentException(format(message, arguments));
+		}
+		return string;
+	}
+
+	private static String format(String message, Object ... arguments) {
+
+		return message;
+	}
+}
