23ae937d266366311fa42265b50f76dc0578293c
Arturo Volpe <avolpe@pol.una.py>
2014-01-10 10:50:11 -0300

    DateXMLAdapter
    Se agrega un XMLAdapter, que convierte de cadenas (desde el XML) a
    objetos del tipo Date, utilizando el DatatypeConverter del API de
    Java.
    
    Esta clase puede ser utilizada conjuntamente con un binding para XJC
    que permita la conversión automática de cadenas a Date, sin utilizar
    la conversión por defecto (que convierte a XMLGregorianCalendar).
    
    Para utilizar, agregar en el archivo de bindings lo siguiente:
    
        <jaxb:globalBindings>
        ...
            <xjc:javaType name="java.util.Date"
                xmlType="xs:date"
                adapter="py.una.med.base.services.util.DateXMLAdapter" />
        ...
        </jaxb:globalBindings>
    
    De esta manera, las clases generadas se verán como:
    
        @XmlElement(required = true, type = String.class)
        @XmlJavaTypeAdapter(DateXMLAdapter.class)
        @XmlSchemaType(name = "date")
        protected Date date;
    
    Lo que le dice a XJC, que el elemento vendrá como una cadena, y que
    será convertido por el objeto DateXMLAdapter.
    
    refs #6747
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>
    Signed-off-by: Hector Martinez <hdmartinez@pol.una.py>

---
+++ src/main/java/py/una/med/base/services/util/DateXMLAdapter.java
@@ -0,0 +1,36 @@
+/*
+ * @DateXMLAdapter.java 1.0 Jan 10, 2014 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.services.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * 
+ * @author Arturo Volpe
+ * @since 1.0
+ * @version 1.0 Jan 10, 2014
+ * 
+ */
+public class DateXMLAdapter extends XmlAdapter<String, Date> {
+
+	@Override
+	public String marshal(Date date) {
+
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(date);
+		return DatatypeConverter.printDate(cal);
+	}
+
+	@Override
+	public Date unmarshal(String s) {
+
+		return DatatypeConverter.parseDate(s).getTime();
+	}
+
+}
