8bedcfb6237094442775489c9f8f47464c6d25bb
Nathalia Ochoa <nochoa@pol.una.py>
2014-02-25 10:35:16 -0300

    Test de controladores
    Se agregan las dependencias de SIGHBaseController en
    ControllerTestConfiguration de manera a evitar su configuración en cada
    uno de los test de controllers.
    
    refs #7564
    
    Signed-off-by: Hector Martinez <hdmartinez@pol.una.py>

--- src/test/java/py/una/med/base/test/configuration/ControllerTestConfiguration.java
+++ src/test/java/py/una/med/base/test/configuration/ControllerTestConfiguration.java
@@ -7,7 +7,13 @@ package py.una.med.base.test.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import py.una.med.base.breadcrumb.BreadcrumbController;
+import py.una.med.base.business.reports.SIGHBaseReportSimple;
+import py.una.med.base.dao.search.SearchHelper;
+import py.una.med.base.jsf.utils.ICurrentpageHelper;
+import py.una.med.base.security.AuthorityController;
 import py.una.med.base.test.util.TestControllerHelper;
+import py.una.med.base.util.DateProvider;
 import py.una.med.base.util.UniqueHelper;
 
 /**
@@ -31,6 +37,42 @@ public class ControllerTestConfiguration extends BaseTestConfiguration {
 	UniqueHelper object() {
 
 		return new UniqueHelper();
+	}
+
+	@Bean
+	public BreadcrumbController breadCrumbController() {
+
+		return null;
+	}
+
+	@Bean
+	public ICurrentpageHelper currentpageHelper() {
+
+		return null;
+	}
+
+	@Bean
+	public SIGHBaseReportSimple baseReportSimple() {
+
+		return null;
+	}
+
+	@Bean
+	public SearchHelper searchHelper() {
+
+		return null;
+	}
+
+	@Bean
+	public AuthorityController authorityController() {
+
+		return null;
+	}
+
+	@Bean
+	public DateProvider dateProvider() {
+	
+		return new DateProvider();
 	}
 
 }
