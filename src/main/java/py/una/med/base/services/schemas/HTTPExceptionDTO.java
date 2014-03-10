c2902271f66ea18c3001abc08cc34db39750632e
Arturo Volpe <avolpe@pol.una.py>
2014-03-10 11:31:35 -0300

    ServicesFault - XSD de excepciones base
    Se agregan esquemas para la excepción base HTTPException, además de la
    clase generada HTTPExceptionDTO, la cual sirve para serializar la
    excepción, y para posteriormente deserializarla.
    
    refs #7896
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>
    Signed-off-by: Hector Martinez <hdmartinez@pol.una.py>

---
+++ src/main/java/py/una/med/base/services/schemas/HTTPExceptionDTO.java
@@ -0,0 +1,120 @@
+
+package py.una.med.base.services.schemas;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="summary" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "code",
+    "summary",
+    "description"
+})
+@XmlRootElement(name = "HTTPException")
+public class HTTPExceptionDTO {
+
+    @XmlElement(required = true)
+    protected String code;
+    @XmlElement(required = true)
+    protected String summary;
+    @XmlElement(required = true)
+    protected String description;
+
+    /**
+     * Gets the value of the code property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Sets the value of the code property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCode(String value) {
+        this.code = value;
+    }
+
+    /**
+     * Gets the value of the summary property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSummary() {
+        return summary;
+    }
+
+    /**
+     * Sets the value of the summary property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSummary(String value) {
+        this.summary = value;
+    }
+
+    /**
+     * Gets the value of the description property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+}
---
+++ src/main/java/py/una/med/base/services/schemas/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://sigh.med.una.py/2013/schemas/base")
+package py.una.med.base.services.schemas;
