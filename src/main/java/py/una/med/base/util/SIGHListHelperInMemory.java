cf38d9778b039268e7f56f42e43f3fdda21a75fa
Claudia Valenzuela <cvalenzuela@pol.una.py>
2014-03-14 17:36:45 -0300

    Administrar Prestación - SIGHListHelperInMemory
    Se agrega la funcionalidad de filtro y paginado al SIGHListHelperInMemory
    
    refs #7522
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>

--- src/main/java/py/una/med/base/util/SIGHListHelperInMemory.java
+++ src/main/java/py/una/med/base/util/SIGHListHelperInMemory.java
@@ -1,22 +1,27 @@
 package py.una.med.base.util;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.faces.model.SelectItem;
 import py.una.med.base.dao.restrictions.Where;
+import py.una.med.base.dao.search.ISearchParam;
 
 /**
- *
- *
+ * Lista de objetos en memoria con paginación.
+ * 
+ * 
+ * 
  * @author Nathalia Ochoa
+ * @author Claudia Valenzuela
  * @since 1.0
  * @version 1.0 23/08/2013
- *
+ * 
  */
 public class SIGHListHelperInMemory<T> implements KarakuListHelperProvider<T> {
 
 	private List<T> listMemory;
 	private SimpleFilter simpleFilter;
+	private PagingHelper pagingHelper;
 
 	public SIGHListHelperInMemory(List<T> list) {
 
@@ -25,14 +30,47 @@ public class SIGHListHelperInMemory<T> implements KarakuListHelperProvider<T> {
 
 	public SIGHListHelperInMemory(SimpleFilter simpleFilter, List<T> list) {
 
-		this.listMemory = list;
+		setListMemory(list);
 		this.simpleFilter = simpleFilter;
+
 	}
 
 	@Override
 	public List<T> getEntities() {
 
-		return listMemory;
+		List<T> toShow = doFilter(listMemory, getSimpleFilter().getOption(),
+				getSimpleFilter().getValue());
+
+		long size = toShow.size();
+		getHelper().udpateCount(size);
+
+		ISearchParam isp = getHelper().getISearchparam();
+		int from = isp.getOffset();
+		int to = isp.getOffset() + isp.getLimit();
+		if (to > listMemory.size()) {
+			to = listMemory.size();
+		}
+
+		return toShow.subList(from, to);
+	}
+
+	/**
+	 * Realiza el filtro de elementos.
+	 * 
+	 * <p>
+	 * Este método es utilizado antes de calcular la paginación y debe retornar
+	 * una sublista de los elementos
+	 * <p>
+	 * 
+	 * 
+	 * @param toFilter
+	 * @param option
+	 * @param value
+	 * @return
+	 */
+	public List<T> doFilter(List<T> toFilter, String option, String value) {
+
+		return toFilter;
 	}
 
 	@Override
@@ -44,7 +82,7 @@ public class SIGHListHelperInMemory<T> implements KarakuListHelperProvider<T> {
 	@Override
 	public List<SelectItem> getFilterOptions() {
 
-		return new ArrayList<SelectItem>();
+		return Collections.emptyList();
 	}
 
 	public List<T> getListMemory() {
@@ -52,8 +90,12 @@ public class SIGHListHelperInMemory<T> implements KarakuListHelperProvider<T> {
 		return listMemory;
 	}
 
-	public void setListMemory(List<T> listMemory) {
+	public final void setListMemory(List<T> listMemory) {
 
+		if (listMemory == null) {
+			setListMemory(Collections.<T> emptyList());
+			return;
+		}
 		this.listMemory = listMemory;
 	}
 
@@ -61,13 +103,16 @@ public class SIGHListHelperInMemory<T> implements KarakuListHelperProvider<T> {
 	public void setBaseWhere(Where<T> where) {
 
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public PagingHelper getHelper() {
 
-		return null;
+		if (pagingHelper == null) {
+			pagingHelper = new PagingHelper(5);
+		}
+
+		return pagingHelper;
 	}
 
 }
