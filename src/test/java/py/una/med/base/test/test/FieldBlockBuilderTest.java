cd1d805d1b1bbdcfdb81bc0d66d505eb4467eb5e
Nathalia Ochoa <nochoa@pol.una.py>
2014-05-28 14:03:43 -0400

    Builder de bloques - Tests
    Se agregan los test de los builders que construyen bloques del tipo
    grilla y del tipo fields.
    
    refs #8844
    
    Signed-off-by: Romina Fernandez <romina.fernandez@pol.una.py>

---
+++ src/test/java/py/una/med/base/test/test/FieldBlockBuilderTest.java
@@ -0,0 +1,109 @@
+/*
+ * @FieldBlockBuilderTest.java 1.0 28/05/2014 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.test.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import py.una.med.base.reports.FieldBlockBuilder;
+import py.una.med.base.reports.SIGHReportBlockField;
+import py.una.med.base.test.base.BaseTest;
+import py.una.med.base.test.configuration.BaseTestConfiguration;
+import py.una.med.base.test.util.TestI18nHelper;
+
+/**
+ * Test del builder que construye bloques de reportes del tipo
+ * fields(label:value).
+ * 
+ * @author Nathalia Ochoa
+ * @since 1.0
+ * @version 1.0 28/05/2014
+ * 
+ */
+
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class FieldBlockBuilderTest extends BaseTest {
+
+	@Configuration
+	static class ContextConfiguration extends BaseTestConfiguration {
+
+	}
+
+	@Autowired
+	private TestI18nHelper i18nHelper;
+
+	@Before
+	public void testBefore() {
+
+		i18nHelper.addString("TITLE", "Bloque con internacionalizacion");
+		i18nHelper.addString("FIELD1", "Nombre");
+		i18nHelper.addString("FIELD2", "Apellido");
+		i18nHelper.addString("PRUEBA", "Lista de personas");
+	}
+
+	@Test
+	public void testCreate() throws Exception {
+
+		FieldBlockBuilder builder1 = new FieldBlockBuilder(
+				"Bloque sin internacionalizacion", false);
+
+		assertEquals("Bloque sin internacionalizacion", builder1.getTitle());
+		assertTrue(builder1.getFields().isEmpty());
+
+		FieldBlockBuilder builder2 = new FieldBlockBuilder("TITLE", true);
+
+		assertEquals("Bloque con internacionalizacion", builder2.getTitle());
+		assertTrue(builder2.getFields().isEmpty());
+	}
+
+	@Test
+	public void testAddField() throws Exception {
+
+		/*
+		 * Caso1. Con internacionalizacion del nombre del label
+		 */
+		FieldBlockBuilder builder1 = new FieldBlockBuilder("TITLE");
+		builder1.addField("FIELD1", true, "GABRIELA");
+		builder1.addField("FIELD2", true, "MEDINA");
+
+		assertEquals(2, builder1.getFields().size());
+		assertEquals("Nombre: ", builder1.getFields().get(0).getLabel());
+		assertEquals("Apellido: ", builder1.getFields().get(1).getLabel());
+
+		/*
+		 * Caso2. Sin internacionalizacion de los nombres de los labels
+		 */
+		FieldBlockBuilder builder2 = new FieldBlockBuilder("TITLE");
+		builder2.addField("Nombre", false, "VIVIANA");
+		builder2.addField("Apellido", false, "OZUNA");
+
+		assertFalse(builder1.getFields().isEmpty());
+		assertEquals(2, builder2.getFields().size());
+		assertEquals("Nombre: ", builder2.getFields().get(0).getLabel());
+		assertEquals("Apellido: ", builder2.getFields().get(1).getLabel());
+	}
+
+	@Test
+	public void testBuild() throws Exception {
+
+		FieldBlockBuilder builder = new FieldBlockBuilder("PRUEBA")
+				.setWidthLabel(20).setWidthValue(50);
+		builder.addField("Nombre: ", false, "CARMEN");
+		builder.addField("Apellido: ", false, "RIVAS");
+
+		SIGHReportBlockField build = builder.build();
+
+		assertEquals("Lista de personas", build.getTitle());
+		assertEquals(20, build.getWidthLabel());
+		assertEquals(50, build.getWidthValue());
+
+	}
+}
---
+++ src/test/java/py/una/med/base/test/test/GridBlockBuilderTest.java
@@ -0,0 +1,132 @@
+/*
+ * @GridBlockBuilderTest.java 1.0 28/05/2014 Sistema Integral de Gestion
+ * Hospitalaria
+ */
+package py.una.med.base.test.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import py.una.med.base.reports.GridBlockBuilder;
+import py.una.med.base.reports.SIGHReportBlockGrid;
+import py.una.med.base.test.base.BaseTest;
+import py.una.med.base.test.configuration.BaseTestConfiguration;
+import py.una.med.base.test.util.TestI18nHelper;
+
+/**
+ * Test del builder que construye bloques de reportes del tipo grilla.
+ * 
+ * @author Nathalia Ochoa
+ * @since 1.0
+ * @version 1.0 28/05/2014
+ * 
+ */
+
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class GridBlockBuilderTest extends BaseTest {
+
+	@Configuration
+	static class ContextConfiguration extends BaseTestConfiguration {
+
+	}
+
+	@Autowired
+	private TestI18nHelper i18nHelper;
+
+	@Before
+	public void testBefore() {
+
+		i18nHelper.addString("TITLE", "Bloque con internacionalizacion");
+		i18nHelper.addString("COLUMNA1", "Contacto");
+		i18nHelper.addString("COLUMNA2", "Telefono");
+		i18nHelper.addString("PRUEBA", "Lista de contactos");
+	}
+
+	@Test
+	public void testCreate() throws Exception {
+
+		GridBlockBuilder builder1 = new GridBlockBuilder(
+				"Bloque sin internacionalizacion", false);
+
+		assertEquals("Bloque sin internacionalizacion", builder1.getTitle());
+		assertTrue(builder1.getColumns().isEmpty());
+
+		GridBlockBuilder builder2 = new GridBlockBuilder("TITLE", true);
+
+		assertEquals("Bloque con internacionalizacion", builder2.getTitle());
+		assertTrue(builder2.getColumns().isEmpty());
+	}
+
+	@Test
+	public void testAddColumn() throws Exception {
+
+		/*
+		 * Caso1. Con internacionalizacion del titulo de cada columna
+		 */
+		GridBlockBuilder builder1 = new GridBlockBuilder("TITLE");
+
+		assertTrue(builder1.getColumns().isEmpty());
+
+		builder1.addColumn("COLUMNA1", true, "nombre");
+		builder1.addColumn("COLUMNA2", true, "apellido");
+
+		assertFalse(builder1.getColumns().isEmpty());
+		assertEquals(2, builder1.getColumns().size());
+		assertEquals("Contacto", builder1.getColumns().get(0).getTitle());
+		assertEquals("Telefono", builder1.getColumns().get(1).getTitle());
+
+		/*
+		 * Caso2. Sin internacionalizacion de los titulos
+		 */
+		GridBlockBuilder builder2 = new GridBlockBuilder("TITLE");
+
+		assertTrue(builder2.getColumns().isEmpty());
+
+		builder2.addColumn("Contacto", false, "contacto");
+		builder2.addColumn("Telefono", false, "telefono");
+
+		assertFalse(builder1.getColumns().isEmpty());
+		assertEquals(2, builder2.getColumns().size());
+		assertEquals("Contacto", builder2.getColumns().get(0).getTitle());
+		assertEquals("Telefono", builder2.getColumns().get(1).getTitle());
+	}
+
+	@Test
+	public void testBuild() throws Exception {
+
+		GridBlockBuilder builder = new GridBlockBuilder("PRUEBA");
+		builder.addColumn("Contacto", false, "contacto");
+		builder.addColumn("Telefono", false, "telefono");
+
+		SIGHReportBlockGrid build = builder.build(getData());
+
+		assertEquals("Lista de contactos", build.getTitle());
+		assertEquals(2, build.getColumns().size());
+
+	}
+
+	private List<String[]> getData() {
+
+		List<String[]> data = new ArrayList<String[]>();
+
+		String[] row = new String[2];
+		row[0] = "LABORATORIO LASCA";
+		row[1] = "(021)620-252";
+
+		row = new String[2];
+		row[0] = "BOTICA MAGISTRAL";
+		row[1] = "(021)900-156";
+		data.add(row);
+
+		return data;
+
+	}
+}
