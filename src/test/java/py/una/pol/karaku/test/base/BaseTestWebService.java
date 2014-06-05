9351a5cb4ed23f8bce4d9c3273d38aad645adf9b
Arturo Volpe <avolpe@pol.una.py>
2014-06-05 10:14:03 -0400

    Agregar header de licencia
    Se agrega el header de la licencia (lgpl2), incluyendo los autores.
    
    refs #9535
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>

--- src/test/java/py/una/pol/karaku/test/base/BaseTestWebService.java
+++ src/test/java/py/una/pol/karaku/test/base/BaseTestWebService.java
@@ -1,5 +1,24 @@
-/*
- * @ExampleTest.java 1.0 Aug 19, 2013 Sistema Integral de Gestion Hospitalaria
+/*-
+ * Copyright (c)
+ *
+ * 		2012-2014, Facultad Politécnica, Universidad Nacional de Asunción.
+ * 		2012-2014, Facultad de Ciencias Médicas, Universidad Nacional de Asunción.
+ * 		2012-2013, Centro Nacional de Computación, Universidad Nacional de Asunción.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 package py.una.pol.karaku.test.base;
 
@@ -19,9 +38,9 @@ import org.springframework.ws.test.server.ResponseMatcher;
 import py.una.pol.karaku.test.configuration.WebServiceTestConfiguration;
 
 /**
- *
+ * 
  * Base clase para Test que simulen llamadas a WebServices.
- *
+ * 
  * <h3>Beans Disponibles</h3>
  * <p>
  * Esta hecha para funcionar en conjunto con {@link WebServiceTestConfiguration}
@@ -34,8 +53,8 @@ import py.una.pol.karaku.test.configuration.WebServiceTestConfiguration;
  * </p>
  * <h3>Métodos Disponibles</h3>
  * <p>
- * La mayoría de los métodos para realizar verificaciónes sobre las respuesta
- * de los servicios se encuentran en
+ * La mayoría de los métodos para realizar verificaciónes sobre las respuesta de
+ * los servicios se encuentran en
  * {@link org.springframework.ws.test.server.RequestCreators} (se recomienda
  * hacer static import de los métodos de esta clase).
  * </p>
@@ -53,52 +72,52 @@ import py.una.pol.karaku.test.configuration.WebServiceTestConfiguration;
  * </p>
  * <h3>Ejemplo de uso</h3>
  * <p>
- *
+ * 
  * <pre>
  * &#064;ContextConfiguration(loader = AnnotationConfigContextLoader.class)
  * public class ClientTest extends BaseTestWebService {
- *
+ * 
  * 	&#064;Configuration
  * 	static class ContextConfiguration extends WebServiceTestConfiguration {
- *
+ * 
  * 		&#064;Override
  * 		(1) public Class&lt;?&gt;[] getClassesToBound() {
- *
+ * 
  * 			return TestUtils.getAsClassArray(MenuRequest.class,
  * 					MenuResponse.class);
  * 		}
- *
- *
+ * 
+ * 
  * 		(2) ServerLogic ServerLogic() {
- *
+ * 
  * 			return new ServerLogic();
  * 		}
- *
- *
+ * 
+ * 
  * 		&#064;Bean
  * 		(3) MenuServiceEndpoint menuServiceEndpoint() {
- *
+ * 
  * 			return new MenuServiceEndpoint();
  * 		}
- *
+ * 
  * 	}
- *
+ * 
  * 	&#064;Test
  * 	public void testCallMocked() {
- *
- *
+ * 
+ * 
  * 		(4) WSRequest wsRequest = new WSRequest();
  * 		(5) sendRequest(wsRequest).
  * 		(6) 	andExpect(noFault());
  * 		(7) MenuResponse response = getResponse();
  * 		(8) assertEquals(&quot;Stock&quot;, response.getMenu().getName());
- *
+ * 
  * 	}
  * }
- *
- *
+ * 
+ * 
  * </pre>
- *
+ * 
  * <h4>Explicación</h4>
  * <ol>
  * <li>Clases que serán marshalizadas o desmarshalizadas</li>
@@ -111,12 +130,12 @@ import py.una.pol.karaku.test.configuration.WebServiceTestConfiguration;
  * <li>Validamos el objeto ya convertido</li>
  * </ol>
  * </p>
- *
+ * 
  * @author Arturo Volpe
  * @since 2.2.8
  * @version 1.0 Oct 18, 2013
  * @see WebServiceTestConfiguration
- *
+ * 
  */
 public abstract class BaseTestWebService extends BaseTest {
 
@@ -147,7 +166,7 @@ public abstract class BaseTestWebService extends BaseTest {
 	 * almacena para poder utilizarla despues mediante el método
 	 * {@link #getResponse()}
 	 * </p>
-	 *
+	 * 
 	 * @param requestCreator
 	 * @return
 	 * @see org.springframework.ws.test.server.MockWebServiceClient#sendRequest(org.springframework.ws.test.server.RequestCreator)
@@ -161,7 +180,7 @@ public abstract class BaseTestWebService extends BaseTest {
 
 	/**
 	 * Serializa un objeto y envía la petición.
-	 *
+	 * 
 	 * <p>
 	 * <b>La clase del objeto debe ser una de las retornadas por
 	 * {@link WebServiceTestConfiguration#getClassesToBound()}</b>
@@ -172,7 +191,7 @@ public abstract class BaseTestWebService extends BaseTest {
 	 * almacena para poder utilizarla después mediante el método
 	 * {@link #getResponse()}
 	 * </p>
-	 *
+	 * 
 	 * @param object
 	 *            objeto a serializar
 	 * @return {@link ResponseActions} para agregar validaciones
@@ -184,12 +203,12 @@ public abstract class BaseTestWebService extends BaseTest {
 
 	/**
 	 * Retorna un {@link ResponseMatcher} que marshaliza el resultado.
-	 *
+	 * 
 	 * <p>
 	 * Este {@link ResponseMatcher} no realiza ninguna acción, siempre valida y
 	 * se encarga de parsear el resultado
 	 * </p>
-	 *
+	 * 
 	 * @return dummy {@link ResponseMatcher}
 	 */
 	public ResponseMatcher responseMarshaller() {
@@ -211,12 +230,12 @@ public abstract class BaseTestWebService extends BaseTest {
 	/**
 	 * Retorna el resultado del ultimo {@link #sendRequest(Object)} válido (en
 	 * este test actual).
-	 *
+	 * 
 	 * <p>
 	 * Para que este método funcione, se debio agregar al menos un
 	 * {@link #responseMarshaller()}
 	 * </p>
-	 *
+	 * 
 	 * @return respuesta marshalizada.
 	 */
 	@SuppressWarnings("unchecked")
@@ -227,12 +246,12 @@ public abstract class BaseTestWebService extends BaseTest {
 
 	/**
 	 * Marshaliza un objeto y lo agrega a un {@link Source}
-	 *
+	 * 
 	 * <p>
 	 * <b>La clase del objeto debe ser una de las retornadas por
 	 * {@link WebServiceTestConfiguration#getClassesToBound()}</b>
 	 * </p>
-	 *
+	 * 
 	 * @param o
 	 *            Objeto a serializar
 	 * @return {@link Source}
--- src/test/java/py/una/pol/karaku/test/test/services/JsonURLProviderTest.java
+++ src/test/java/py/una/pol/karaku/test/test/services/JsonURLProviderTest.java
@@ -1,6 +1,24 @@
-/*
- * @JsonURLProviderTset.java 1.0 Oct 21, 2013 Sistema Integral de Gestion
- * Hospitalaria
+/*-
+ * Copyright (c)
+ *
+ * 		2012-2014, Facultad Politécnica, Universidad Nacional de Asunción.
+ * 		2012-2014, Facultad de Ciencias Médicas, Universidad Nacional de Asunción.
+ * 		2012-2013, Centro Nacional de Computación, Universidad Nacional de Asunción.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 package py.una.pol.karaku.test.test.services;
 
