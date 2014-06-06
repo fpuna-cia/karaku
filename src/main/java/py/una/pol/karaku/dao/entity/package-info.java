8962866ea580d34ffcf3b45d773a52b477c9c88a
Arturo Volpe <avolpe@pol.una.py>
2014-06-06 08:53:49 -0400

    Agregar licencia a archivos package-info.java
    Los archivos del tipo package-info.java (principalmente utilizados para
    documentación de paquetes) no fueron afectados por la ejecución
    automática del plugin JAutodoc (el mismo agregaba los headers a cada
    archivo), se procede a agregarlos manualmente.
    
    refs #9535
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>

--- src/main/java/py/una/pol/karaku/dao/entity/package-info.java
+++ src/main/java/py/una/pol/karaku/dao/entity/package-info.java
@@ -1,9 +1,28 @@
-/*
- * @package-info.java 1.0 Oct 1, 2013 Sistema Integral de Gestion Hospitalaria
+/*-
+ * Copyright (c)
+ *
+ * 		2012-2014, Facultad Politécnica, Universidad Nacional de Asunción.
+ * 		2012-2014, Facultad de Ciencias Médicas, Universidad Nacional de Asunción.
+ * 		2012-2013, Centro Nacional de Computación, Universidad Nacional de Asunción.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 /**
  * Clases que proveen funcionalidades extras a entidades.
- *
+ * 
  * <p>
  * Se divide en tres secciones:
  * <ul>
@@ -14,10 +33,11 @@
  * <li>Types: tipos no estándar que soporta Karaku</li>
  * </ul>
  * </p>
- *
+ * 
  * @author Arturo Volpe
  * @since 1.0
  * @version 1.0 Oct 1, 2013
- *
+ * 
  */
 package py.una.pol.karaku.dao.entity;
+
--- src/test/java/py/una/pol/karaku/test/test/util/layers/package-info.java
+++ src/test/java/py/una/pol/karaku/test/test/util/layers/package-info.java
@@ -1,19 +1,38 @@
-/*
- * @package-info.java 1.0 Sep 10, 2013 Sistema Integral de Gestion Hospitalaria
+/*-
+ * Copyright (c)
+ *
+ * 		2012-2014, Facultad Politécnica, Universidad Nacional de Asunción.
+ * 		2012-2014, Facultad de Ciencias Médicas, Universidad Nacional de Asunción.
+ * 		2012-2013, Centro Nacional de Computación, Universidad Nacional de Asunción.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 /**
- * Paquete que incluye una version simulado de todas las capas de la aplicaciÃ³n.
- * <h3>Incluye las siguientes capas</h3>
+ * Paquete que incluye una version simulado de todas las capas de la
+ * aplicaciÃ³n. <h3>Incluye las siguientes capas</h3>
  * <ol>
  * <li> {@link py.una.pol.karaku.test.util.TestEntity} representando el nivel de
  * entidades</li>
  * <li> {@link py.una.pol.karaku.test.util.TestDAO} representando los daos.</li>
  * <li> {@link py.una.pol.karaku.test.util.TestLogic} represntando a la lÃ³gica</li>
  * </ol>
- *
+ * 
  * @author Arturo Volpe
  * @since 2.2
  * @version 1.0 Sep 10, 2013
- *
+ * 
  */
 package py.una.pol.karaku.test.test.util.layers;
