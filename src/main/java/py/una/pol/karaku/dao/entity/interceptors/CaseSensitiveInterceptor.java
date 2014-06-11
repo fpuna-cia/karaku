c410c5aaa2a1a2276861ed27788476533f2bfdb4
Arturo Volpe <avolpe@pol.una.py>
2014-06-11 14:45:17 -0400

    CaseSensitiveInterceptor des-habilitable
    Se agrega un mecanismo para deshabilitar el CaseSensitiveInterceptor,
    para realizarlo, se debe agregar la llave:
    
        karaku.interceptor.caseSensitive
    
    En el `karaku.properties` con valor `false`. Esto permite que no todos
    los sistemas que utilicen karaku tengan que forzar a las cadenas a ser
    mayúsculas.
    
    refs #9550
    
    Signed-off-by: Arturo Volpe <avolpe@pol.una.py>

--- src/main/java/py/una/pol/karaku/dao/entity/interceptors/CaseSensitiveInterceptor.java
+++ src/main/java/py/una/pol/karaku/dao/entity/interceptors/CaseSensitiveInterceptor.java
@@ -24,29 +24,52 @@ package py.una.pol.karaku.dao.entity.interceptors;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
+import py.una.pol.karaku.configuration.PropertiesUtil;
 import py.una.pol.karaku.dao.annotations.CaseSensitive;
 import py.una.pol.karaku.dao.entity.Operation;
 
 /**
- *
+ * 
  * @author Arturo Volpe
  * @since 1.0
  * @version 1.0 Oct 1, 2013
- *
+ * 
  */
 @Component
 public class CaseSensitiveInterceptor extends AbstractInterceptor {
 
+	@Autowired
+	private PropertiesUtil properties;
+
+	boolean enabled;
+
+	@PostConstruct
+	public void postConstruct() {
+
+		enabled = properties.getBoolean("karaku.interceptor.caseSensitive",
+				true);
+	}
+
 	@Override
 	public Class<?>[] getObservedTypes() {
+
+		if (!enabled) {
+			return new Class<?>[] {};
+		}
 
 		return new Class<?>[] { String.class };
 	}
 
 	@Override
 	public Class<?>[] getObservedAnnotations() {
+
+		if (!enabled) {
+			return new Class<?>[] {};
+		}
 
 		return new Class<?>[] { CaseSensitive.class, void.class };
 
@@ -55,8 +78,8 @@ public class CaseSensitiveInterceptor extends AbstractInterceptor {
 	@Override
 	public boolean interceptable(Operation op, Field f, Object bean) {
 
-		boolean interceptable = (op != Operation.DELETE)
-				&& (f.getAnnotation(CaseSensitive.class) == null);
+		boolean interceptable = op != Operation.DELETE
+				&& f.getAnnotation(CaseSensitive.class) == null;
 		for (Annotation a : f.getAnnotations()) {
 			if (a.annotationType().isAnnotationPresent(CaseSensitive.class)) {
 				interceptable = false;
