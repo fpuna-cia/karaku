c3727294fb92be9e5fc2d22ace537c66f24e02e6
Saúl Zalimben <szalimben@pol.una.py>
2015-02-27 12:59:01 -0300

    Modificación de LazyModel, LazyModelTest, LazyModelTest.aql
    Se modificaron los archivos por conflictos en Base con los
    demas modulos. En LazyModel se incluyo para busqueda para
    tipo Long, se modifico LazyModelTest para el coverage y
    LazyModelTest.sqp los datos para las pruebas.
    
    Refs #12847
    
    Signed-off-by: Saúl Zalimben <szalimben@pol.una.py>

--- src/test/java/py/una/pol/karaku/test/util/LazyModelTest.java
+++ src/test/java/py/una/pol/karaku/test/util/LazyModelTest.java
@@ -14,86 +14,95 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import py.una.med.asistencial.domain.Cama;
-import py.una.med.asistencial.domain.persona.Persona;
-import py.una.med.configuracion.business.IUniversidadLogic;
-import py.una.med.configuracion.business.UniversidadLogic;
-import py.una.med.configuracion.dao.IUniversidadDao;
-import py.una.med.configuracion.dao.UniversidadDao;
-import py.una.med.configuracion.domain.Universidad;
+import py.una.pol.karaku.survey.business.EncuestaLogic;
+import py.una.pol.karaku.survey.business.EncuestaPlantillaLogic;
+import py.una.pol.karaku.survey.business.IEncuestaPlantillaLogic;
+import py.una.pol.karaku.survey.domain.Encuesta;
+import py.una.pol.karaku.survey.domain.EncuestaPlantilla;
+import py.una.pol.karaku.survey.repo.EncuestaDAO;
+import py.una.pol.karaku.survey.repo.EncuestaDetalleDAO;
+import py.una.pol.karaku.survey.repo.EncuestaDetalleOpcionRespuestaDAO;
+import py.una.pol.karaku.survey.repo.EncuestaPlantillaDAO;
 import py.una.pol.karaku.test.base.BaseTestWithDatabase;
 import py.una.pol.karaku.test.configuration.ControllerTestConfiguration;
+import py.una.pol.karaku.test.configuration.TestBeanCreator;
 import py.una.pol.karaku.test.configuration.TransactionTestConfiguration;
-import py.una.pol.karaku.test.util.transaction.SQLFiles;
 import py.una.pol.karaku.util.LazyModel;
 
-@SQLFiles
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 public class LazyModelTest extends BaseTestWithDatabase {
 
 	@Configuration
 	static class ContextConfiguration extends ControllerTestConfiguration {
-		
+
 		@Bean
-		public LazyModel<Universidad, Long> lazyModel() {
-			return new LazyModel<Universidad, Long>();
-				 
-		} 
+		public LazyModel<EncuestaPlantilla, Long> lazyModel() {
+			return new LazyModel<EncuestaPlantilla, Long>();
+
+		}
 	}
-	
+
 	@Configuration
 	static class ContextConfiguration2 extends TransactionTestConfiguration {
-		
-		
+
 		@Bean
-		public IUniversidadLogic UniversidadLogic() {
-			
-			return new UniversidadLogic();
+		public TestBeanCreator beanCreator() {
+
+			return new TestBeanCreator(TestUtils.getAsClassArray(
+					EncuestaLogic.class, EncuestaDetalleDAO.class,
+					EncuestaPlantillaLogic.class,
+					EncuestaPlantillaDAO.class,
+					EncuestaDAO.class, EncuestaDetalleOpcionRespuestaDAO.class));
 		}
-		
-		@Bean
-		public IUniversidadDao UniversidadDao() {
-			
-			return new UniversidadDao();
-		}
-		
+
 		@Override
 		public Class<?>[] getEntityClasses() {
 
-			return TestUtils.getReferencedClasses(
-					Cama.class,
-					Persona.class,
-					Universidad.class);
+			return TestUtils.getReferencedClasses(Encuesta.class,
+					EncuestaPlantilla.class);
 		}
 	}
 
 	@Autowired
-	private IUniversidadLogic logic;
+	private IEncuestaPlantillaLogic logic;
 
 	@Autowired
 	private TestControllerHelper controllerHelper;
 
 	@Autowired
-	private LazyModel<Universidad, Long> lazyModel;
-	
+	private LazyModel<EncuestaPlantilla, Long> lazyModel;
+
 	@Test
 	public void getLoad() {
 
 		int pageSize = 10;
 		int first = 0;
 		Map<String, Object> filters = new HashMap<String, Object>();
-		String sortField = "sigla";
-		String value = "UNA";
-		String nameField = "sigla";
-		
+
+		String sortField = "id";
+		Long value = 1L;
+		String nameField = "id";
+
 		filters.put(nameField, value);
-		
+
 		lazyModel.setLogic(logic);
+
+		List<EncuestaPlantilla> Encuestaes = lazyModel.load(first, pageSize, sortField,
+				SortOrder.ASCENDING, filters);
+
+		assertNotNull(Encuestaes);
 		
-		List<Universidad> universidades = lazyModel.load(first, pageSize, sortField, SortOrder.ASCENDING, filters);
+		sortField = "usuario";
+		String valor = "";
+		nameField = "usuario";
+
+		filters.put(nameField, valor);
 		
-		assertNotNull(universidades);
+		List<EncuestaPlantilla> Encuestas = lazyModel.load(first, pageSize, sortField,
+				SortOrder.ASCENDING, filters);
+
+		assertNotNull(Encuestas);
+		
 	}
-	
-	
+
 }
