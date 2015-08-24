991b4839c99bd65c79ca6e718e9dea7e2295dae4
Yohanna Lisnichuk <ylisnichuk@pol.una.py>
2015-08-24 11:20:41 -0400

    Authority Controller - Usuario en mayúsculas
    * Se modifican los métodos getUsername y getUsernameStatic en
      la clase AuthorityController para que retorne el nombre de usuario
      del usuario actualmente logueado pero en mayúsculas siempre.
    
    Refs #16853
    Refs #17096
    
    Signed-off-by: Yohanna Lisnichuk <ylisnichuk@pol.una.py>

--- src/main/java/py/una/pol/karaku/security/AuthorityController.java
+++ src/main/java/py/una/pol/karaku/security/AuthorityController.java
@@ -45,70 +45,71 @@ import py.una.pol.karaku.util.StringUtils;
 @Service
 public class AuthorityController {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(AuthorityController.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(AuthorityController.class);
 
-	/**
-	 * Método que se utiliza como punto único de acceso para la seguridad en
-	 * cuento a vistas, esto es, todas las vistas deberían preguntar a este
-	 * controller, si se tienen los permisos adecuados para visualizar un
-	 * elemento, esto se hace mediante el tag, &lt security &gt.<br>
-	 * Funciona buscando en la session el usuario actual de la aplicación,
-	 * obtiene la autenticación, y un vector de autoridades (
-	 * {@link KarakuPermission}), compara el permiso que da cada una de
-	 * esas autoridades con el permiso solicitado, si no tiene el permiso
-	 * retorna falso, y loguea un mensaje de error
-	 * 
-	 * @param rol
-	 *            a probar
-	 * @return true si tiene permiso, false en otro caso
-	 */
-	public boolean hasRole(String rol) {
+    /**
+     * Método que se utiliza como punto único de acceso para la seguridad en
+     * cuento a vistas, esto es, todas las vistas deberían preguntar a este
+     * controller, si se tienen los permisos adecuados para visualizar un
+     * elemento, esto se hace mediante el tag, &lt security &gt.<br>
+     * Funciona buscando en la session el usuario actual de la aplicación,
+     * obtiene la autenticación, y un vector de autoridades (
+     * {@link KarakuPermission}), compara el permiso que da cada una de esas
+     * autoridades con el permiso solicitado, si no tiene el permiso retorna
+     * falso, y loguea un mensaje de error
+     * 
+     * @param rol
+     *            a probar
+     * @return true si tiene permiso, false en otro caso
+     */
+    public boolean hasRole(String rol) {
 
-		return hasRoleStatic(rol);
-	}
+        return hasRoleStatic(rol);
+    }
 
-	/**
-	 * Retorna el usuario que actualmente esta autenticado en el sistema.
-	 * 
-	 * @return {@link String} representando el nombre del usuario.
-	 */
-	@Nonnull
-	public String getUsername() {
+    /**
+     * Retorna el usuario que actualmente esta autenticado en el sistema.
+     * 
+     * @return {@link String} representando el nombre del usuario.
+     */
+    @Nonnull
+    public String getUsername() {
 
-		return notNull(SecurityContextHolder.getContext().getAuthentication()
-				.getName(), "Can't get the current username");
-	}
+        return notNull(SecurityContextHolder.getContext().getAuthentication()
+                .getName().toUpperCase(), "Can't get the current username");
+    }
 
-	/**
-	 * Retorna el usuario que actualmente esta autenticado en el sistema.
-	 * 
-	 * @return {@link String} representando el nombre del usuario.
-	 * @see #getUsername()
-	 */
-	public static String getUsernameStatic() {
+    /**
+     * Retorna el usuario que actualmente esta autenticado en el sistema.
+     * 
+     * @return {@link String} representando el nombre del usuario.
+     * @see #getUsername()
+     */
+    public static String getUsernameStatic() {
 
-		return SecurityContextHolder.getContext().getAuthentication().getName();
-	}
+        return SecurityContextHolder.getContext().getAuthentication().getName()
+                .toUpperCase();
+    }
 
-	/**
-	 * Igual que {@link #hasRole(String)}, solamente que es un método estático,
-	 * lo que permite a clases que no están en el mismo alcance (
-	 * {@link WebApplicationContext#SCOPE_SESSION}) puedan realizar controles de
-	 * permisos.
-	 * 
-	 * @param rol
-	 *            nombre del permiso.
-	 * @return <code>true</code> si se encuentra el permiso, <code>false</code>
-	 *         en caso contrario.
-	 */
-	public static boolean hasRoleStatic(String rol) {
+    /**
+     * Igual que {@link #hasRole(String)}, solamente que es un método estático,
+     * lo que permite a clases que no están en el mismo alcance (
+     * {@link WebApplicationContext#SCOPE_SESSION}) puedan realizar controles de
+     * permisos.
+     * 
+     * @param rol
+     *            nombre del permiso.
+     * @return <code>true</code> si se encuentra el permiso, <code>false</code>
+     *         en caso contrario.
+     */
+    public static boolean hasRoleStatic(String rol) {
 
-		if (StringUtils.isInvalid(rol)) {
-			LOG.trace("Checking a invalid rol ({})", rol);
-		}
-		return ((KarakuUser) SecurityContextHolder.getContext()
-				.getAuthentication().getPrincipal()).hasRole(rol);
+        if (StringUtils.isInvalid(rol)) {
+            LOG.trace("Checking a invalid rol ({})", rol);
+        }
+        return ((KarakuUser) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal()).hasRole(rol);
 
-	}
+    }
 }
